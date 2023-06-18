ab5f40c8295e8954b4b9d5e04d581908282de286
udairhythm <undoshola@gmail.com>
2023-06-18 17:23:57 +0900

    フォルダの場所変更

---
+++ manappi_php/input.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>課題テンプレート</title>
+</head>
+
+<body>
+    <form action="write.php" method="post">
+        お名前: <input type="text" name="name">
+        出身: <input type="text" name="birthPlace">
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
---
+++ manappi_php/write.php
@@ -0,0 +1,35 @@
+<?php
+// inputから受け取る
+$name = $_POST['name'];
+$birthPlace = $_POST['birthPlace'];
+
+// データを整形する
+$data = $name . $birthPlace . "\n";
+
+// データを保存する
+file_put_contents('data/data.txt', $data, FILE_APPEND)
+
+//文字作成
+
+?>
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>File書き込み</title>
+</head>
+
+<body>
+
+    <h1>書き込みしました。</h1>
+    <h2>./data/data.txt を確認しましょう！</h2>
+
+    <ul>
+        <li><a href="read.php">確認する</a></li>
+        <li><a href="input.php">戻る</a></li>
+    </ul>
+</body>
+
+</html>
